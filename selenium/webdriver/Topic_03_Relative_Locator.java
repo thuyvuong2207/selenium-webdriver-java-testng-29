db29c222c83e8a1b5b2925a2ecb407156c267009
vmt <vuongminhthuy97@gmail.com>
2024-03-03 11:35:03 +0700

    [Demo lesson] Topic 08 - XPath/CSS pt I

---
+++ selenium/webdriver/Topic_03_Relative_Locator.java
@@ -0,0 +1,68 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+//import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_03_Relative_Locator {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+        }
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Relative() {
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2Fregister");
+
+        // Login (Above on UI)
+        // Loginbutton option 1
+        By loginButtonBy = By.cssSelector("button.login-button");
+        // Loginbutton option 2
+        WebElement loginButtonElement = driver.findElement(By.cssSelector("button.login-button"));
+
+        RelativeLocator.with(By.tagName("label"))
+                .above(loginButtonBy);
+        RelativeLocator.with(By.tagName("label"))
+                .above(loginButtonElement);
+
+        // Remember me checkbox ( on UI)
+        By rememberMeCheckbox = By.id("RememberMe");
+        WebElement rememberMeTextElement = driver
+                .findElement(RelativeLocator.with(By.tagName("label"))
+                .above(loginButtonBy)
+                .toRightOf(rememberMeCheckbox));
+        System.out.println(rememberMeTextElement.getText());
+    }
+
+// find relative locator: above login button and right of rememberMeCheckbox
+//        RelativeLocator.with(By.tagName("label"))
+//                .above(loginButtonBy)
+//                .toRightOf(rememberMeCheckbox);
+//    }
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
