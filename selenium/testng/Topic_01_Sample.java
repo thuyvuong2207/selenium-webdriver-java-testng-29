0d6f9c9921213f3c3d99b846e218c2cde4ddf1c9
vmt <vuongminhthuy97@gmail.com>
2024-06-01 10:22:28 +0700

    TestNG - topic 1,2

--- selenium/testng/Topic_01_Sample.java
+++ selenium/testng/Topic_01_Sample.java
@@ -1,2 +1,23 @@
-package testng;public class Topic_01_Sample {
+package testng;
+
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Topic_01_Sample {
+
+        // Unit tests
+        @Test
+        public void testGetRandomNumber() {
+            Topic_01_Sample sample = new Topic_01_Sample();
+            int randomNumber = sample.getRandomNumber();
+            Assert.assertTrue(randomNumber >= 0 && randomNumber < 1000000);
+        }
+
+    // Component
+    private int getRandomNumber(){
+        return new Random().nextInt(1000000);
+    }
 }
--- selenium/testng/Topic_02_Annotation.java
+++ selenium/testng/Topic_02_Annotation.java
@@ -1,2 +1,56 @@
-package testng;public class Topic_02_Annotation {
+package testng;
+
+import org.testng.annotations.*;
+
+public class Topic_02_Annotation {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("BeforeClass");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("AfterClass");
+    }
+
+    @BeforeGroups
+    public void beforeGroups() {
+        System.out.println("BeforeGroups");
+    }
+
+    @AfterGroups
+    public void afterGroups() {
+        System.out.println("afterGroups");
+    }
+
+    @Test
+    public void test01() {
+        System.out.println("Test 01");
+    }
+    @Test
+    public void test02() {
+        System.out.println("Test 02");
+    }
+    @Test (enabled = false, description = "ID Jira ticket")
+    public void test03() {
+        System.out.println("Test 03");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before Suite");
+    }
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After Suite");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before Method");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After Method");
+    }
 }
